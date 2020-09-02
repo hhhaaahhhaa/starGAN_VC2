cfc520b7b872f23b8f32353116948baef2ec09ab
hhhaaahhhaa <thomas1232121@gmail.com>
2020-09-02 11:28:23 +0800

    Update results

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python3 inference.py -a vctk/trimmed_vctk_spectrograms/sr_24000_mel_norm/attr.pkl -c config.yaml -m model/adaptive_vc/vctk_model.ckpt -s $1 -t $2 -o $3
+python3 inference.py -a vctk/trimmed_vctk_spectrograms/sr_24000_mel_norm/attr.pkl -c config.yaml -m model_upload/vctk_model_19999.ckpt -s $1 -t $2 -o $3
