eb9214c3b2ec98344d9f200b6f7f272b3373786c
hhhaaahhhaa <thomas1232121@gmail.com>
2020-09-04 01:26:27 +0800

    version 0

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 main.py -c config.yaml -d vctk/trimmed_vctk_spectrograms/sr_24000_mel_norm -train_set train_128 -train_index_file train_samples_128.json -store_model_path model/adaptive_vc/vctk_model -t vctk_model -iters 500000 -summary_step 500
+python3 main.py -c config.yaml -d vctk/trimmed_vctk_spectrograms/sr_24000_mel_norm -train_set train_128 -train_index_file train_samples_128.json -store_model_path model/adaptive_vc -t vctk_model -iters 500000 -summary_step 500
