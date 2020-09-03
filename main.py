299533d45c8fa6541ebc6ef8380bfe1c1addd4fe
hhhaaahhhaa <thomas1232121@gmail.com>
2020-09-04 01:24:03 +0800

    version 0

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ if __name__ == '__main__':
     parser.add_argument('--load_opt', action='store_true')
     parser.add_argument('-store_model_path', default='/storage/model/adaptive_vc/model')
     parser.add_argument('-load_model_path', default='/storage/model/adaptive_vc/model')
+    parser.add_argument('-log_step', default=1, type=int)
     parser.add_argument('-summary_steps', default=100, type=int)
-    parser.add_argument('-save_steps', default=5000, type=int)
+    parser.add_argument('-save_steps', default=20, type=int)
     parser.add_argument('-tag', '-t', default='init')
     parser.add_argument('-iters', default=0, type=int)
 
